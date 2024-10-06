6f82f9ffebffda7c0e0a5244d441acf80fe5bc28
KelanaIrsyad <irsyadkelana@gmail.com>
2024-10-06 19:04:47 +0700

    Stop tracking .env file

--- app/server.go
+++ app/server.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 )
 
@@ -14,8 +16,14 @@ type Server struct {
 	Router *mux.Router
 }
 
-func (server *Server) Initialize() {
-	fmt.Println("welcome to GoToko")
+type AppConfig struct {
+	AppName string
+	AppEnv  string
+	AppPort string
+}
+
+func (server *Server) Initialize(appConfig AppConfig) {
+	fmt.Println("welcome to " + appConfig.AppName)
 
 	server.Router = mux.NewRouter()
 	server.initializeRoutes()
@@ -28,7 +36,17 @@ func (server *Server) Run(addr string) {
 
 func Run() {
 	var server = Server{}
+	var appConfig = AppConfig{}
 
-	server.Initialize()
-	server.Run(":9000")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error on loading .env file")
+	}
+
+	appConfig.AppName = os.Getenv("APP_NAME")
+	appConfig.AppEnv = os.Getenv("APP_ENV")
+	appConfig.AppPort = os.Getenv("APP_PORT")
+
+	server.Initialize(appConfig)
+	server.Run(":" + appConfig.AppPort)
 }
